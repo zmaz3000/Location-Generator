134e0f2869d752e6d12491c0490c09d99c66cd7a
zmaz3000 <122127730+zmaz3000@users.noreply.github.com>
2023-02-07 21:55:44 -0600

    Div container and responsiveness

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ var myChoice = Math.random() * 10;
 var randomIndexedNumber = Math.round(myChoice, 2);
 var btn = document.querySelector(".generatebutton");
 var reload = document.querySelector(".reload");
-var myInput = document.querySelector(".familyinput");
+var myInput = document.querySelector(".family");
+var myDiv = document.getElementById("myDiv");
 reload.style.display = "none";
 
 btn.addEventListener("click", function(){
     if(myInput.value != ""){
         var myElement = document.createElement("h2");
         myElement.innerHTML = cities[randomIndexedNumber];
-        document.body.appendChild(myElement);
-        console.log(myElement);
+        myDiv.appendChild(myElement);
+        console.log(myDiv);
         btn.disabled = true;
         myInput.disabled = true;
         reload.style.display = "block";
